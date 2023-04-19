d366ac14bf4a4d877e5be969902d9fdbb707c194
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-19 21:15:16 +0545

    feat(main): Add search box widget to the window
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, SearchEntry};
 
 const APP_ID: &str = "org.gtk_rs.la";
 
@@ -16,6 +16,16 @@ fn build_window(app: &Application) {
         .height_request(50)
         .width_request(500)
         .resizable(false)
+        .child(&build_widgets_box())
         .build();
     window.present();
+}
+
+/// Builds the container box with all the widgets necessary for an app
+fn build_widgets_box() -> gtk::Box {
+    let search_box = SearchEntry::builder().hexpand(true).build();
+    let gtk_box = gtk::Box::builder().build();
+    gtk_box.append(&search_box);
+
+    gtk_box
 }
