9514e54cf711c8c5db0903c787e30a3bf1082014
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-19 20:52:53 +0545

    feat(main): Implement basic window builder code
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.la";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_window);
+    app.run()
+}
+
+fn build_window(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("la")
+        .height_request(50)
+        .width_request(500)
+        .resizable(false)
+        .build();
+    window.present();
 }
