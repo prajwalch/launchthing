2dacf494fe603e2ec16c354eb0df0f4ea3513a0a
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-23 21:36:30 +0545

    refactor(all): Implement new `SearchWindow` for managing window
    Also this features implements a new functionality that allows seach box
    to emit search action whenever search changes and search results
    container builder `SearchWindow.build_search_results_container()` to
    connect on that action
    
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,16 @@
 mod search_results;
+mod search_window;
 
 use gtk::glib;
-use gtk::glib::clone;
 use gtk::prelude::*;
 
 const APP_ID: &str = "com.prajwalch.la";
 
 fn main() -> glib::ExitCode {
     let app = gtk::Application::builder().application_id(APP_ID).build();
-    app.connect_activate(build_window);
+    app.connect_activate(|app| {
+        let window = search_window::SearchWindow::new(app);
+        window.present();
+    });
     app.run()
-}
-
-fn build_window(app: &gtk::Application) {
-    gtk::ApplicationWindow::builder()
-        .application(app)
-        .title("la")
-        .height_request(50)
-        .width_request(500)
-        .resizable(false)
-        .child(&build_box_of_all_widgets())
-        .build()
-        .present();
-}
-
-/// Builds the container box of all the widgets necessary for an app
-fn build_box_of_all_widgets() -> gtk::Box {
-    let container = gtk::Box::builder()
-        .orientation(gtk::Orientation::Vertical)
-        .build();
-    let search_box = gtk::SearchEntry::builder().hexpand(true).build();
-    let search_results = search_results::SearchResults::new();
-
-    search_box.connect_search_changed(
-        clone!(@weak container, @weak search_results => move |term| {
-            let term = term.text();
-            let matched_terms = get_matched_terms(&term);
-
-            if term.is_empty() || matched_terms.is_empty() {
-                search_results.clear_all();
-                return;
-            }
-            for term in matched_terms {
-                // TODO: Instead of passing individual text change it to take matched terms directly
-                search_results.append_new(gtk::Text::builder().text(term).build());
-            }
-        }),
-    );
-    container.append(&search_box);
-    container.append(&search_results);
-    container
-}
-
-fn get_matched_terms(term: &str) -> Vec<&str> {
-    let fake = vec!["Obs", "Clion", "Vim", "Nvim", "Visual Studio Code"];
-
-    if fake.contains(&term) {
-        fake
-    } else {
-        vec![]
-    }
 }
---
+++ src/search_window.rs
@@ -0,0 +1,85 @@
+use gtk::gio;
+use gtk::glib::{self, clone};
+use gtk::prelude::*;
+
+#[derive(Clone)]
+pub struct SearchWindow {
+    window: gtk::ApplicationWindow,
+    container: gtk::Box,
+}
+
+impl SearchWindow {
+    pub fn new(app: &gtk::Application) -> Self {
+        let window = gtk::ApplicationWindow::new(app);
+        window.set_height_request(50);
+        window.set_width_request(500);
+        window.set_resizable(false);
+
+        let container = gtk::Box::new(gtk::Orientation::Vertical, 5);
+        window.set_child(Some(&container));
+
+        Self { window, container }
+    }
+
+    pub fn present(&self) {
+        self.container.append(&self.build_search_box_widget());
+        self.container
+            .append(&self.build_search_results_container());
+        self.window.present();
+    }
+
+    fn build_search_box_widget(&self) -> gtk::SearchEntry {
+        let search_box = gtk::SearchEntry::builder().hexpand(true).build();
+        search_box.connect_search_changed(move |search_box| {
+            let search_query = search_box.text().to_string();
+            search_box
+                .activate_action("win.search", Some(&search_query.to_variant()))
+                .expect("search action should exist");
+        });
+        search_box
+    }
+
+    fn build_search_results_container(&self) -> crate::search_results::SearchResults {
+        let search_results = crate::search_results::SearchResults::new();
+        let search_action = gio::SimpleAction::new("search", Some(&String::static_variant_type()));
+
+        search_action.connect_activate(clone!(@weak search_results => move |_state, variant| {
+            if let Some(variant) = variant {
+                let search_query = variant.get::<String>().unwrap_or_default();
+
+                if search_query.is_empty() {
+                    // Clear previous results
+                    search_results.clear_all();
+                    return;
+                }
+
+                // FIXME: If two queries (ex: `ab` and `abc`) gives same result than avoid same
+                //        results from being pushed
+                if let Some(results) = get_matched_terms(&search_query) {
+                   for result in results {
+                        search_results.append_new(gtk::Text::builder().text(&result).build());
+                    }
+                }
+            }
+        }));
+        self.window.add_action(&search_action);
+        search_results
+    }
+}
+
+// TODO: Remove this function
+fn get_matched_terms(term: &str) -> Option<Vec<String>> {
+    let fake = vec!["Obs", "Clion", "Vim", "Nvim", "Visual Studio Code"];
+
+    let matched_terms = fake
+        .iter()
+        .filter(|f| f.matches(term).count() != 0)
+        .map(|f| f.to_string())
+        .collect::<Vec<String>>();
+
+    if !matched_terms.is_empty() {
+        Some(matched_terms)
+    } else {
+        None
+    }
+}
