51fabb4e5bbc80a7005e0e847145429db72d3ec2
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-24 20:50:30 +0545

    feat(search_window): Implement basic actual apps search functionality
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/search_window.rs
+++ src/search_window.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use gtk::gio;
 use gtk::glib::{self, clone};
 use gtk::prelude::*;
@@ -6,6 +8,7 @@ use gtk::prelude::*;
 pub struct SearchWindow {
     window: gtk::ApplicationWindow,
     container: gtk::Box,
+    installed_apps: Rc<Vec<gio::AppInfo>>,
 }
 
 impl SearchWindow {
@@ -18,7 +21,11 @@ impl SearchWindow {
         let container = gtk::Box::new(gtk::Orientation::Vertical, 5);
         window.set_child(Some(&container));
 
-        Self { window, container }
+        Self {
+            window,
+            container,
+            installed_apps: Rc::new(gio::AppInfo::all()),
+        }
     }
 
     pub fn present(&self) {
@@ -40,6 +47,7 @@ impl SearchWindow {
     }
 
     fn build_search_results_container(&self) -> crate::search_results::SearchResults {
+        let installed_apps = Rc::clone(&self.installed_apps);
         let search_results = crate::search_results::SearchResults::new();
         let search_action = gio::SimpleAction::new("search", Some(&String::static_variant_type()));
 
@@ -52,31 +60,23 @@ impl SearchWindow {
                 if search_query.is_empty() {
                     return;
                 }
-                if let Some(results) = get_matched_terms(&search_query) {
-                    for result in results {
-                        search_results.push(gtk::Text::builder().text(&result).build());
+                let query_matched_apps = installed_apps.iter().filter_map(|app| {
+                    let app_name = app.name();
+                    if app_name.matches(&search_query).count() != 0 {
+                        Some(app_name)
+                    } else {
+                        None
+                    }
+                }).collect::<Vec<glib::GString>>();
+
+                if !query_matched_apps.is_empty() {
+                    for app_name in &query_matched_apps {
+                        search_results.push(gtk::Text::builder().text(app_name.as_str()).build());
                     }
                 }
             }
         }));
         self.window.add_action(&search_action);
         search_results
-    }
-}
-
-// TODO: Remove this function
-fn get_matched_terms(term: &str) -> Option<Vec<String>> {
-    let fake = vec!["Obs", "Clion", "Vim", "Nvim", "Visual Studio Code"];
-
-    let matched_terms = fake
-        .iter()
-        .filter(|f| f.matches(term).count() != 0)
-        .map(|f| f.to_string())
-        .collect::<Vec<String>>();
-
-    if !matched_terms.is_empty() {
-        Some(matched_terms)
-    } else {
-        None
     }
 }
