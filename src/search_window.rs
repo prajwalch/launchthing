b7a15ecf2eab1817720cefe835c6d6ded5de51e3
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-26 20:15:58 +0545

    feat(search_window): Make search results scrollable
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/search_window.rs
+++ src/search_window.rs
@@ -8,6 +8,7 @@ use crate::search_results::SearchResults;
 
 pub struct SearchWindow {
     window: gtk::ApplicationWindow,
+    scrolled_container: gtk::ScrolledWindow,
     search_results: SearchResults,
     installed_apps: Rc<Vec<gio::AppInfo>>,
 }
@@ -22,12 +23,19 @@ impl SearchWindow {
         let container = gtk::Box::new(gtk::Orientation::Vertical, 5);
         container.append(&Self::create_search_box_widget());
 
+        let scrollable_container = gtk::ScrolledWindow::new();
+        scrollable_container.set_min_content_height(200);
+        // Only show it when we get the results later
+        scrollable_container.set_visible(false);
+
         let search_results = SearchResults::new();
-        container.append(&search_results);
+        scrollable_container.set_child(Some(&search_results));
+        container.append(&scrollable_container);
         window.set_child(Some(&container));
 
         Self {
             window,
+            scrolled_container: scrollable_container,
             search_results,
             installed_apps: Rc::new(gio::AppInfo::all()),
         }
@@ -50,35 +58,42 @@ impl SearchWindow {
     }
 
     fn create_search_action(&self) -> gio::SimpleAction {
-        let installed_apps = Rc::clone(&self.installed_apps);
+        let scrolled_container = &self.scrolled_container;
         let search_results = &self.search_results;
+        let installed_apps = Rc::clone(&self.installed_apps);
         let search_action = gio::SimpleAction::new("search", Some(&String::static_variant_type()));
 
-        search_action.connect_activate(clone!(@weak search_results => move |_state, variant| {
-            if let Some(variant) = variant {
-                // Clear previous results
-                search_results.clear();
+        search_action.connect_activate(
+            clone!(@weak search_results, @weak scrolled_container => move |_state, variant| {
+                if let Some(variant) = variant {
+                    // Clear previous results
+                    search_results.clear();
+                    scrolled_container.hide();
 
-                let search_query = variant.get::<String>().unwrap_or_default();
-                if search_query.is_empty() {
-                    return;
-                }
-                let query_matched_apps = installed_apps.iter().filter_map(|app| {
-                    let app_name = app.name();
-                    if app_name.matches(&search_query).count() != 0 {
-                        Some(app_name)
-                    } else {
-                        None
+                    let search_query = variant.get::<String>().unwrap_or_default();
+                    if search_query.is_empty() {
+                        return;
                     }
-                }).collect::<Vec<glib::GString>>();
+                    let query_matched_apps = installed_apps.iter().filter_map(|app| {
+                        let app_name = app.name();
+                        if app_name.matches(&search_query).count() != 0 {
+                            Some(app_name)
+                        } else {
+                            None
+                        }
+                    }).collect::<Vec<glib::GString>>();
 
-                if !query_matched_apps.is_empty() {
+                    if query_matched_apps.is_empty() {
+                        return;
+                    }
+
                     for app_name in &query_matched_apps {
                         search_results.push(gtk::Text::builder().text(app_name.as_str()).build());
                     }
+                    scrolled_container.show();
                 }
-            }
-        }));
+            }),
+        );
         search_action
     }
 }
