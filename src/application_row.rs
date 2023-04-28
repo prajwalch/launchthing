379981ec61173dba53927e5adae09ae5aa62c5ad
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-04-28 20:36:24 +0545

    feat: Implement custom widget for showing app name, icon and description
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

---
+++ src/application_row.rs
@@ -0,0 +1,35 @@
+use gtk::gio;
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+
+mod imp;
+
+glib::wrapper! {
+    pub struct ApplicationRow(ObjectSubclass<imp::ApplicationRow>)
+        @extends gtk::Widget, gtk::Box;
+}
+
+impl Default for ApplicationRow {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ApplicationRow {
+    pub fn new() -> Self {
+        glib::Object::new()
+    }
+
+    pub fn set_info(&self, app_info: &gio::AppInfo) {
+        let imp = self.imp();
+        imp.name.set_text(&app_info.name());
+
+        if let Some(description) = app_info.description() {
+            imp.description.set_text(&description);
+        }
+        if let Some(icon) = app_info.icon() {
+            imp.icon.set_from_gicon(&icon);
+        }
+    }
+}
