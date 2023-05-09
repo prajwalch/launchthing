c84a4ac893a3a0c632bcf28c8cf13141335b5cd1
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-05-09 20:41:22 +0545

    feat(main): Implement css loader to use css in later
    Main benefit of using css is that it makes customization easy for any
    widget like changing font size, border radius etc.
    
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod path_results;
 mod search_results;
 mod search_window;
 
+use gtk::gdk;
 use gtk::glib;
 use gtk::prelude::*;
 
@@ -12,9 +13,23 @@ fn main() -> glib::ExitCode {
     let app = gtk::Application::builder().application_id(APP_ID).build();
     app.set_accels_for_action("window.close", &["Escape"]);
 
+    app.connect_startup(|_| load_css());
     app.connect_activate(|app| {
         let window = search_window::SearchWindow::new(app);
         window.present();
     });
     app.run()
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = gtk::CssProvider::new();
+    provider.load_from_data(include_str!("style/app.css"));
+
+    // Add the provider to the default screen
+    gtk::style_context_add_provider_for_display(
+        &gdk::Display::default().expect("Display should exist"),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
