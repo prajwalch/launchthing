86763e4f1ea9c6517ffbdd6559aefed780ab788a
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-05-12 18:07:33 +0545

    feat(path_results): Use different icon for each kind of file
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/path_results.rs
+++ src/path_results.rs
@@ -1,3 +1,4 @@
+use std::borrow::Borrow;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
@@ -54,15 +55,113 @@ fn create_list_item(child_path: &Path) -> ListItem {
     ListItem::builder().child(&container).build()
 }
 
-fn create_icon_widget(_path: &Path) -> gtk::Image {
-    // TODO: Use different kind of icon for different kind of file extension
-    let icon = gtk::Image::from_icon_name("folder");
+fn create_icon_widget(path: &Path) -> gtk::Image {
+    let icon = gtk::Image::from_icon_name(get_icon_name_from_path(path));
     icon.set_margin_top(2);
     icon.set_margin_bottom(2);
     icon.set_margin_start(10);
     icon.set_margin_end(2);
     icon.set_pixel_size(25);
     icon
+}
+
+fn get_icon_name_from_path(path: &Path) -> &str {
+    if path.is_dir() {
+        return "folder";
+    }
+
+    let Some(file_extension) = path.extension() else {
+        return "application-x-executable";
+    };
+
+    match file_extension.to_string_lossy().borrow() {
+        // application
+        "default" => "application-octet-stream",
+        "abw" => "application-x-abiword",
+        "arc" => "application-x-freearc",
+        "azw" => "application-vnd.amazon.ebook",
+        "bin" => "application-octet-stream",
+        "bz" => "application-x-bzip",
+        "bz2" => "application-x-bzip2",
+        "cda" => "application-x-cdf",
+        "csh" => "application-x-csh",
+        "doc" => "application-msword",
+        "docx" => "application-vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "eot" => "application-vnd.ms-fontobject",
+        "epub" => "application-epub+zip",
+        "gz" => "application-gzip",
+        "jar" => "application-java-archive",
+        "json" => "application-json",
+        "jsonld" => "application-ld+json",
+        "mpkg" => "application-vnd.apple.installer+xml",
+        "odp" => "application-vnd.oasis.opendocument.presentation",
+        "ods" => "application-vnd.oasis.opendocument.spreadsheet",
+        "odt" => "application-vnd.oasis.opendocument.text",
+        "ogx" => "application-ogg",
+        "pdf" => "application-pdf",
+        "php" => "application-x-httpd-php",
+        "ppt" => "application-vnd.ms-powerpoint",
+        "pptx" => "application-vnd.openxmlformats-officedocument.presentationml.presentation",
+        "rar" => "application-vnd.rar",
+        "rtf" => "application-rtf",
+        "sh" => "application-x-sh",
+        "tar" => "application-x-tar",
+        "vsd" => "application-vnd.visio",
+        "xhtml" => "application-xhtml+xml",
+        "xls" => "application-vnd.ms-excel",
+        "xlsx" => "application-vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        "xml" => "application-xml",
+        "xul" => "application-vnd.mozilla.xul+xml",
+        "zip" => "application-zip",
+        "7z" => "application-x-7z-compressed",
+
+        // audio
+        "aac" => "audio-aac",
+        "mid" => "audio-midi",
+        "midi" => "audio-x-midi",
+        "mp3" => "audio-mpeg",
+        "oga" => "audio-ogg",
+        "opus" => "audio-opus",
+        "wav" => "audio-wav",
+        "weba" => "audio-webm",
+
+        // font
+        "otf" => "font-otf",
+        "woff" => "font-woff",
+        "woff2" => "font-woff2",
+
+        // image
+        "avif" => "image-avif",
+        "bmp" => "image-bmp",
+        "gif" => "image-gif",
+        "ico" => "image-vnd.microsoft.icon",
+        "jpeg" | "jpg" => "image-jpeg",
+        "png" => "image-png",
+        "svg" => "image-svg+xml",
+        "tif" | "tiff" => "image-tiff",
+        "webp" => "image-webp",
+
+        // text
+        "css" => "text-css",
+        "csv" => "text-csv",
+        "htm" | "html" => "text-html",
+        "ics" => "text-calendar",
+        "js" | "mjs" => "text-javascript",
+        "txt" => "text-plain",
+
+        // video
+        "avi" => "video-x-msvideo",
+        "mkv" => "video-x-generic",
+        "mp4" => "video-mp4",
+        "mpeg" => "video-mpeg",
+        "ogv" => "video-ogg",
+        "ts" | "ttf" => "video-mp2t",
+        "webm" => "video-webm",
+        "3gp" => "video-3gpp",
+        "3g2" => "video-3gpp2",
+
+        _ => "unknown",
+    }
 }
 
 fn create_label_widget(path_name: &str) -> gtk::Label {
