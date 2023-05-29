9e78d4c4cf85178c41dc80ca19910742158eb841
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-05-29 19:47:38 +0545

    chore(modes): Add full stop `.`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/modes.rs
+++ src/modes.rs
@@ -21,16 +21,16 @@ pub trait Mode {
     /// Checks if mode is activated from the given query.
     ///
     /// Different modes can have different keywords for activating them.
-    /// For eg: A mode named `finder` may activate if query starts with `/`
+    /// For eg: A mode named `finder` may activate if query starts with `/`.
     fn is_activated(query: &str) -> bool;
 
-    /// Checks if any data is present for creating list items
+    /// Checks if any data is present for creating list items.
     fn contains_data(&self) -> bool;
 
-    /// Creates list items by binding the data
+    /// Creates list items by binding the data.
     fn create_list_items(&self) -> Vec<ListItem>;
 
-    /// Callback for when an item is selected by user
+    /// Callback for when an item is selected by user.
     fn on_item_selected(&self, item: &ListItem);
 }
 
