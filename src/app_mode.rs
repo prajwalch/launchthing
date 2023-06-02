c587ec012a77ba342c92b8046fb217f0c06c08ad
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-06-02 20:53:17 +0545

    refactor(app_mode): Directly pass app description to constructor
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/app_mode.rs
+++ src/app_mode.rs
@@ -173,13 +173,12 @@ fn create_name_and_description_widget(app: &gio::AppInfo) -> gtk::Box {
     container.append(&name);
 
     if let Some(app_des) = app.description() {
-        let description = gtk::Label::new(None);
+        let description = gtk::Label::new(Some(&app_des));
         description.set_halign(gtk::Align::Start);
         description.set_margin_top(2);
         description.set_opacity(0.5);
         description.set_wrap(true);
         description.set_css_classes(&["body"]);
-        description.set_text(&app_des);
 
         container.append(&description);
     }
